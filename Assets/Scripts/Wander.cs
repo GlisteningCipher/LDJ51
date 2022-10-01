1d5ec134f8949cd02ad9e35f0cf8d338e85327e0
Justin Law <jlawhome08@gmail.com>
2022-10-01 01:17:39 -0700

    basic guest wandering behavior

---
+++ Assets/Scripts/Wander.cs
@@ -0,0 +1,92 @@
+using UnityEngine;
+using MonsterLove.StateMachine;
+using System.Collections;
+
+public class Wander : MonoBehaviour
+{
+    [SerializeField] float speed = 1f;
+    [SerializeField] Rigidbody2D rb;
+
+    static float ROOM_HALFWIDTH = 16;
+    static float ROOM_HALFHEIGHT = 10;
+
+    public enum State { Idle, Move }
+
+    public class Driver
+    {
+        public StateEvent FixedUpdate;
+    }
+
+    public StateMachine<State, Driver> fsm;
+
+    Vector2 destination;
+
+    Coroutine idleIfStuck;
+
+    void Awake()
+    {
+        fsm = new StateMachine<State, Driver>(this);
+        fsm.ChangeState(State.Idle);
+    }
+
+    void OnEnable()
+    {
+        fsm.ChangeState(State.Idle);
+    }
+
+    void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+
+    void FixedUpdate()
+    {
+        fsm.Driver.FixedUpdate.Invoke();
+    }
+
+    IEnumerator Idle_Enter()
+    {
+        rb.drag = 10;
+        yield return new WaitForSeconds(2);
+        GetRandomDestination();
+        fsm.ChangeState(State.Move);
+    }
+
+    void Move_Enter()
+    {
+        rb.drag = 2;
+        idleIfStuck = StartCoroutine(IdleIfStuck());
+    }
+
+    IEnumerator IdleIfStuck()
+    {
+        yield return new WaitForSeconds(5);
+        fsm.ChangeState(State.Idle);
+    }
+
+    void Move_FixedUpdate()
+    {
+        var distance = (destination - rb.position);
+        if (distance.magnitude < 0.1f)
+        {
+            fsm.ChangeState(State.Idle);
+            return;
+        }
+
+        var direction = distance.normalized;
+        rb.AddForce(direction * speed);
+    }
+
+    void Move_Exit()
+    {
+        StopCoroutine(idleIfStuck);
+    }
+
+    void GetRandomDestination()
+    {
+        destination = new Vector2(
+            Random.Range(-ROOM_HALFWIDTH, ROOM_HALFWIDTH),
+            Random.Range(-ROOM_HALFHEIGHT, ROOM_HALFHEIGHT));
+    }
+    
+}
