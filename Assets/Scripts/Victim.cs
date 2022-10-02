669e0c2271fa159d6e15398c152c1d8e1bd59d77
Justin Law <jlawhome08@gmail.com>
2022-10-01 17:30:16 -0700

    prevent getting stuck on corpse
    static body might end up trapping a guest on a corpse

--- Assets/Scripts/Victim.cs
+++ Assets/Scripts/Victim.cs
@@ -15,7 +15,7 @@ public class Victim : MonoBehaviour
     void Die()
     {
         transform.parent.localScale = new Vector3(1f, 0.25f, 1f);
-        GetComponentInParent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+        GetComponentInParent<Rigidbody2D>().mass = 9999;
         Destroy(GetComponentInParent<Wander>());
         Destroy(gameObject);
     }
